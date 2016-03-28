8b370f024ba9af6b0beaeada7359261fbb4600fc
John Paul Alcala <ore.wa.dragon.lord@gmail.com>
2016-03-28 19:38:06 +0800

    Cleanup and changes to the dockerfile and the startup script.
    For one, the activemq config is now copied during image creation,
    and the data volume is now /var/lib/activemq/data. This simplifies
    a lot of things.

--- activemq-entrypoint.sh
+++ activemq-entrypoint.sh
@@ -2,10 +2,6 @@
 set -e
 
 run_activemq() {
-    if [ $# -eq 0 ] && { ! find $ACTIVEMQ_BASE -mindepth 1 -print -quit | grep -q .; }; then
-        mkdir -p $ACTIVEMQ_BASE/data && cp -rf $ACTIVEMQ_HOME/conf $ACTIVEMQ_BASE
-    fi
-
     chown -R activemq:activemq $ACTIVEMQ_BASE
 
     exec gosu activemq /usr/local/bin/activemq ${@:-console}
